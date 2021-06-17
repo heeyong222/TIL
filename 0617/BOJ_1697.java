67d3274900624bb4964be4cefac7f74ad4560fb8
heeyong222 <laru1072@gmail.com>
2021-06-18 07:04:44 +0900

    0617

---
+++ 0617/BOJ_1697.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1697 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+    }
+}
